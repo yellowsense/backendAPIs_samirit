dec82fececaa291b53bb5c0d8811ab648f89f565
yellowsense <148525405+yellowsense@users.noreply.github.com>
2023-11-15 19:51:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS, cross_origin
 import pyodbc
 from datetime import time
 
-app = Flask(__name__)
+app = Flask(_name_)
 CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 
@@ -46,7 +46,7 @@ def get_maidreg_data():
         maidreg_data = []
         for row in rows:
             maid = {
-                "ID": row.ID,  # Add this line to include the "ID" key
+                "ID": row.ID,
                 "Name": row.Name,
                 "Gender": row.Gender,
                 "Services": row.Services.split(','),
@@ -56,44 +56,50 @@ def get_maidreg_data():
             maidreg_data.append(maid)
         return maidreg_data
     except pyodbc.Error as e:
+        app.logger.error("Error fetching maidreg data: %s", e)
         return []
 
-
+# Fetch maid data only once when the application starts
 service_providers = get_maidreg_data()
 
 def find_matching_service_providers(Locations, Services, date, start_time_str):
     start_time = parse_time_string(start_time_str)
     if start_time is None:
+        app.logger.error("Invalid start_time format: %s", start_time_str)
         return {"error": "Invalid start_time format"}
+    
     matching_providers = []
     for provider in service_providers:
-        if any(service in Services for service in provider["Services"]):
-            if any(location in Locations for location in provider["Locations"]):
-                Timings = provider["Timings"].split(',')
-                for timing_range in Timings:
-                    start_str, end_str = timing_range.split('-')
-                    start = parse_time_string(start_str)
-                    end = parse_time_string(end_str)
-                    print(f"Start: {start}, End: {end}, Start Time: {start_time}")
-                    if start is None or end is None:
-                        return {"error": "Invalid timing format"}
-                    if start <= start_time < end:
-                        matching_providers.append({"ID": provider["ID"], **provider})
-                        break
+        # Check if the specified location is in the provider's list of locations
+        if isinstance(provider["Locations"], list) and Locations.lower() in [loc.strip().lower() for loc in provider["Locations"]]:
+            # Check if the specified service is in the provider's list of services
+            if Services.lower() in [serv.strip().lower() for serv in provider["Services"]]:
+                # Check if Timings is a valid list
+                if ',' in provider["Timings"]:
+                    Timings = provider["Timings"].split(',')
+                    for timing_range in Timings:
+                        start_str, end_str = timing_range.split('-')
+                        start = parse_time_string(start_str)
+                        end = parse_time_string(end_str)
+                        if start is None or end is None:
+                            app.logger.error("Invalid timing format in provider %s: %s", provider["ID"], timing_range)
+                            continue
+                        if start <= start_time < end:
+                            matching_providers.append({"ID": provider["ID"], **provider})
+                            break  # Break from the inner loop once a match is found for this provider
+                else:
+                    app.logger.error("Invalid Timings format in provider %s: %s", provider["ID"], provider["Timings"])
     return matching_providers
-
 
 @app.route('/get_matching_service_providers', methods=['GET', 'POST'])
 @cross_origin()
 def get_matching_providers():
     if request.method == 'GET':
-        # Extract parameters from the URL for GET requests
         Locations = request.args.get('Locations')
         Services = request.args.get('Services')
         date = request.args.get('date')
         start_time = request.args.get('start_time')
     elif request.method == 'POST':
-        # Extract parameters from the JSON body for POST requests
         data = request.json
         Locations = data.get('Locations')
         Services = data.get('Services')
@@ -112,106 +118,6 @@ def get_matching_providers():
     else:
         return jsonify({"providers": "No matching service providers found"})
 
-@app.route('/society_names', methods=['GET'])
-@cross_origin()
-def get_society_names():
-    try:
-        # Execute a SQL query to retrieve society names and IDs
-        cursor.execute("SELECT society_id, society_name FROM Society")
-        rows = cursor.fetchall()
 
-        # Convert the result into an array of dictionaries with id and name
-        society_data = [{"id": row.society_id, "name": row.society_name} for row in rows]
-
-        return jsonify(society_data)  # Return JSON with id and name
-    except pyodbc.Error as e:
-        return jsonify({"error": str(e)})
-
-@app.route('/insert_maid', methods=['POST'])
-@cross_origin()
-def insert_maid():
-    try:
-        # Extract parameters from the JSON body for POST requests
-        data = request.json
-        aadhar_number = data.get('AadharNumber')
-        name = data.get('Name')
-        phone_number = data.get('PhoneNumber')
-        gender = data.get('Gender')
-        services = data.get('Services')
-        locations = data.get('Locations')
-        timings = data.get('Timings')
-
-        # Execute the stored procedure
-        cursor = conn.cursor()
-        cursor.execute(
-            "EXEC InsertMaidRegistration "
-            "@AadharNumber = ?, "
-            "@Name = ?, "
-            "@PhoneNumber = ?, "
-            "@Gender = ?, "
-            "@Services = ?, "
-            "@Locations = ?, "
-            "@Timings = ?",
-            (aadhar_number, name, phone_number, gender, services, locations, timings)
-        )
-        conn.commit()
-        cursor.close()
-
-        # Return a success message
-        return jsonify({"message": "Maid entry added successfully"})
-    except Exception as e:
-        # Log the error and return an error message in case of an exception
-        app.logger.error(str(e))
-        return jsonify({"error": "Internal Server Error"}), 500
-
-@app.route('/get_all_maid_details', methods=['GET'])
-@cross_origin()
-def get_all_maid_details():
-    try:
-        cursor.execute("SELECT * FROM maidreg")
-        rows = cursor.fetchall()
-
-        maid_details_list = []
-        for row in rows:
-            maid_details = {
-                "ID": row.ID,
-                "AadharNumber": row.AadharNumber,
-                "Name": row.Name,
-                "PhoneNumber": row.PhoneNumber,
-                "Gender": row.Gender,
-                "Services": row.Services.split(','),
-                "Locations": row.Locations.split(','),
-                "Timings": row.Timings
-            }
-            maid_details_list.append(maid_details)
-
-        return jsonify({"maid_details": maid_details_list})
-    except pyodbc.Error as e:
-        return jsonify({"error": str(e)})
-
-@app.route('/get_maid_details/<int:maid_id>', methods=['GET'])
-@cross_origin()
-def get_maid_details(maid_id):
-    try:
-        cursor.execute("SELECT * FROM maidreg WHERE ID=?", (maid_id,))
-        row = cursor.fetchone()
-
-        if row:
-            maid_details = {
-                "ID": row.ID,
-                "AadharNumber": row.AadharNumber,
-                "Name": row.Name,
-                "PhoneNumber": row.PhoneNumber,
-                "Gender": row.Gender,
-                "Services": row.Services.split(','),
-                "Locations": row.Locations.split(','),
-                "Timings": row.Timings
-            }
-            return jsonify(maid_details)
-        else:
-            return jsonify({"error": "Maid not found"})
-    except pyodbc.Error as e:
-        return jsonify({"error": str(e)})
-
-if __name__ == '__main__':
+if _name_ == '_main_':
     app.run(debug=True)
