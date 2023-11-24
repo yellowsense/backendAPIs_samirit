49e7905d2853dd447a67cff3204a85ac3e1f3d0e
yellowsense <148525405+yellowsense@users.noreply.github.com>
2023-11-24 14:13:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -291,29 +291,30 @@ def login_maid():
     try:
         # Extract parameters from the JSON body for POST requests
         data = request.json
-        email_id = data.get('EmailID')
+        username = data.get('Username')
         password = data.get('Password')
 
-        # Execute the SQL query to check if the provided email_id and password match
+        # Execute the SQL query to check if the provided username and password match for maid_accountdetails
+        cursor = conn.cursor()
         cursor.execute(
-            "SELECT * FROM maid_accountdetails WHERE EmailID = ? AND Password = ?",
-            (email_id, password)
+            "SELECT * FROM dbo.maid_accountdetails WHERE Username = ? AND Password = ?",
+            (username, password)
         )
         row = cursor.fetchone()
 
         if row:
-            # Maid details found, return the details
+            # Maid account details found, return the details
             maid_details = {
                 "MaidID": row.MaidID,
-                "Name": row.Name,
+                "Username": row.Username,
                 "MobileNumber": row.MobileNumber,
-                "EmailID": row.EmailID
-                # Add other fields if needed
+                "EmailID": row.EmailID,
+                "MaidRegID": row.MaidRegID
             }
             return jsonify(maid_details)
         else:
-            # No matching maid found
-            return jsonify({"error": "Invalid email or password"}), 401
+            # No matching maid account found
+            return jsonify({"error": "Invalid username or password"}), 401
     except Exception as e:
         # Log the error and return an error message in case of an exception
         app.logger.error(str(e))
@@ -325,34 +326,33 @@ def login_customer():
     try:
         # Extract parameters from the JSON body for POST requests
         data = request.json
-        email_id = data.get('EmailID')
+        username = data.get('Username')
         password = data.get('Password')
 
-        # Execute the SQL query to check if the provided email_id and password match
+        # Execute the SQL query to check if the provided username and password match for customer_accountdetails
+        cursor = conn.cursor()
         cursor.execute(
-            "SELECT * FROM customer_accountdetails WHERE EmailID = ? AND Password = ?",
-            (email_id, password)
+            "SELECT * FROM dbo.customer_accountdetails WHERE Username = ? AND Password = ?",
+            (username, password)
         )
         row = cursor.fetchone()
 
         if row:
-            # Customer details found, return the details
+            # Customer account details found, return the details
             customer_details = {
                 "CustomerID": row.CustomerID,
-                "Name": row.Name,
+                "Username": row.Username,
                 "MobileNumber": row.MobileNumber,
                 "EmailID": row.EmailID
-                # Add other fields if needed
             }
             return jsonify(customer_details)
         else:
-            # No matching customer found
-            return jsonify({"error": "Invalid email or password"}), 401
+            # No matching customer account found
+            return jsonify({"error": "Invalid username or password"}), 401
     except Exception as e:
         # Log the error and return an error message in case of an exception
         app.logger.error(str(e))
         return jsonify({"error": "Internal Server Error"}), 500
-
 
 if __name__ == '__main__':
     app.run(debug=True)
